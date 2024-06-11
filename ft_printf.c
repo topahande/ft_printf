24e2b76a8c8c249c2de264dfb2294d82f2e66c9f
topahande <ladytastingdata@gmail.com>
2024-06-11 19:40:37 +0300

    Improving the function for edge cases with single % at the end of string

--- ft_printf.c
+++ ft_printf.c
@@ -16,24 +16,27 @@ static int	print_formatted(const char *format, va_list args)
 {
 	int	count;
 
+	count = 0;
 	if (*format == 'd' || *format == 'i')
 		count = ft_putnbr(va_arg(args, int));
-	if (*format == 'c')
+	else if (*format == 'c')
 		count = ft_putchar(va_arg(args, int));
-	if (*format == 's')
+	else if (*format == 's')
 		count = ft_putstr(va_arg(args, char *));
-	if (*format == '%')
+	else if (*format == '%')
 		count = ft_putpercent();
-	if (*format == 'u')
+	else if (*format == 'u')
 		count = ft_putunsignednbr(va_arg(args, unsigned int));
-	if (*format == 'X')
+	else if (*format == 'X')
 		count = ft_puthexadecimal(va_arg(args, unsigned int),
 				"0123456789ABCDEF");
-	if (*format == 'x')
+	else if (*format == 'x')
 		count = ft_puthexadecimal(va_arg(args, unsigned int),
 				"0123456789abcdef");
-	if (*format == 'p')
+	else if (*format == 'p')
 		count = ft_putpointer(va_arg(args, void *));
+	else
+		count = write(1, &format[0], 1);
 	return (count);
 }
 
@@ -49,8 +52,13 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			format++;
-			check_write = print_formatted(format, args);
+			if (*(format + 1) == '\0')
+				check_write = 0;
+			else
+			{
+				format++;
+				check_write = print_formatted(format, args);
+			}
 		}
 		else
 			check_write = ft_putchar(*format);
